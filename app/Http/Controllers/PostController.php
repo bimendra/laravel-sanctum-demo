4ebe86ca8d1f84052ef8ddea9edc56058317857f
= <=>
2025-01-27 16:56:32 +0800

    Apply sanctum middleware to posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class PostController extends Controller
+class PostController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show']),
+        ];
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -21,7 +30,7 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        return Post::create($request->validated());
+        return $request->user()->posts()->create($request->validated());
     }
 
     /**
