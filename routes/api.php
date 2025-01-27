eccb74851aedb7e22c8cbab86bd935fc56fcc460
= <=>
2025-01-27 16:07:38 +0800

    User authentication & token generation

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('post', App\Http\Controllers\PostController::class);
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:sanctum');
